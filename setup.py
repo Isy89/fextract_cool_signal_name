45a404f3f89a87ca2ca632c9867f350f84c8e8aa
isy89 <lazzeri89@gmail.com>
2024-06-03 15:50:11 +0200

    - update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requires = []
+    for line in f:
+        req = line.split("#", 1)[0].strip()
+        requires.append(req)
+
 setup(
     name="fextract_cool_signal_name",
+    install_requires=requires,
     entry_points={"lbfextract": [
         "cool_signal_name = fextract_cool_signal_name.plugin:hook"
     ],
